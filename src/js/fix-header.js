2949561f376434fe4eedd256c29f92f84131380e
“Евгений <kea-asd@yandex.ru>
2022-05-11 00:54:57 +0700

    fix

--- src/js/fix-header.js
+++ src/js/fix-header.js
@@ -2,25 +2,6 @@ const header = document.querySelector('.header');
 const page = document.querySelector('main');
 let links = document.querySelectorAll('.links');
 const nodeList = document.querySelectorAll(".modal__link");
-console.log(nodeList[1]);
-// for (let i = 0; i < nodeList.length; i++) {
-//   nodeList[i].style.backgroundColor = "red";
-//   if (nodeList[i].click()) {
-//     console.log("linki");
-    
-//   };
-// }
-
-nodeList[1].onclick = () => {
-  // for (let i = 0; i < nodeList.length; i++) {
-    // if (i < 8) {
-      console.log("click");
-      header.classList.remove('fix-header');
-    }
-   
-
-
-
  // scroll 
 if(window.innerWidth <= 800 ) {
 function fixHeader() {
